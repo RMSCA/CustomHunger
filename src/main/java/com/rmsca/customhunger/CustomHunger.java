f7b13efb5292975e6f74959cbc669cacc54150fa
Chu <yuzhu@amazon.com>
2022-06-21 10:55:02 -0400

    Initial commit

---
+++ src/main/java/com/rmsca/customhunger/CustomHunger.java
@@ -0,0 +1,18 @@
+package com.rmsca.customhunger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomHunger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+}
